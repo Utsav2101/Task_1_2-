d6d61c8830b4a5ec441e655d4e9a04ff2ba7ba73
Utsav2101 <113006733+Utsav2101@users.noreply.github.com>
2022-09-26 19:50:57 +0530

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,1 +1,14 @@
+num = int(input("Enter the limit to find the fibonacci series : "))
+num1 = 0
+num2 = 1
+i = 0
+while (i < num):
+    if(i <= 1):
+        next = i
+    else:
+        next = num1 + num2
+        num1 = num2
+        num2 = next
+    print(next)
+    i = i+1
 
