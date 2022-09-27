9d8988b515d6344994cd31f37edbf4c656953251
Utsav2101 <113006733+Utsav2101@users.noreply.github.com>
2022-09-27 11:53:47 +0530

    Update Task_5.py
    Word frequencies in words

--- Task_5.py
+++ Task_5.py
@@ -1,1 +1,13 @@
+def word_frequencies(words):
+    freq = {}
+    for word in words:
+        freq[word] = words.count(word)
+    return freq
+
+if __name__ == '__main__':
+    words = input("Enter youe word here :")
+    your_dictionary = word_frequencies(words)
+    sorted_keys = sorted(your_dictionary.keys())
+    for key in sorted_keys:
+        print(key + ':' + str(your_dictionary[key]))
 
